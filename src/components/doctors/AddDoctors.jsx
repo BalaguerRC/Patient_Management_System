d092191e2fa0a884d0636a2622c55a398e6bb0f4
BalaguerRC <86633462+BalaguerRC@users.noreply.github.com>
2023-07-18 11:05:38 -0400

    adddoctors and EditDoctor

--- src/components/doctors/AddDoctors.jsx
+++ src/components/doctors/AddDoctors.jsx
@@ -1,9 +1,40 @@
 import { Button, Grid, Paper, TextField } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddDoctors = () => {
+  const [Name, setName] = useState();
+  const [LastName, setLastName] = useState();
+  const [Mail, setMail] = useState();
+  const [Phone, setPhone] = useState();
+  const [Identity, setIdentity] = useState();
+  const [Image, setImage] = useState("https://assets.stickpng.com/images/5a4613ddd099a2ad03f9c994.png");
+
+  const navigate = useNavigate()
+
+  const token = localStorage.getItem("token_user");
+
+  const AddDoctors=(Name,LastName,Mail,Phone,Identity,Image)=>{
+    //console.log(Name,LastName,Mail,Phone,Identity,Image)
+    fetch(import.meta.env.VITE_APIURL + "Doctors", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/Json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+        name_Doctor: Name,
+        lastName_Doctor: LastName,
+        email_Doctor: Mail,
+        phone_Doctor: Phone,
+        identity_Doctor: Identity,
+        img_Doctor: Image
+      })}).then(res=>res.json()).then(data=>console.log(data))
+  }
   return (
     <div>
       AddDoctors
+      <Button onClick={() => navigate("/doctors")}>{"<-"}Back</Button>
       <Paper>
         <Grid
           container
@@ -11,14 +42,14 @@ const AddDoctors = () => {
           justifyContent={"center"}
           alignItems={"center"}
         >
-          <TextField type="text" placeholder="name..." fullWidth />
-          <TextField type="text" placeholder="lastname..." fullWidth />
-          <TextField type="text" placeholder="mail..." fullWidth />
-          <TextField type="text" placeholder="phone..." fullWidth />
-          <TextField type="text" placeholder="identity..." fullWidth />
+          <TextField type="text" placeholder="name..." fullWidth onChange={(e)=>setName(e.target.value)}/>
+          <TextField type="text" placeholder="lastname..." fullWidth onChange={(e)=>setLastName(e.target.value)}/>
+          <TextField type="text" placeholder="mail..." fullWidth onChange={(e)=>setMail(e.target.value)}/>
+          <TextField type="text" placeholder="phone..." fullWidth onChange={(e)=>setPhone(e.target.value)}/>
+          <TextField type="text" placeholder="identity..." fullWidth onChange={(e)=>setIdentity(e.target.value)}/>
           <TextField type="file"/>
 
-          <Button>Save</Button>
+          <Button onClick={()=>AddDoctors(Name,LastName,Mail,Phone,Identity,Image)}>Save</Button>
         </Grid>
       </Paper>
     </div>
--- src/components/doctors/EditDoctor.jsx
+++ src/components/doctors/EditDoctor.jsx
@@ -1,13 +1,68 @@
-import { useParams } from 'react-router-dom';
+import { Button, Grid, Paper, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const EditDoctor = () => {
+  const [Name, setName] = useState();
+  const [LastName, setLastName] = useState();
+  const [Mail, setMail] = useState();
+  const [Phone, setPhone] = useState();
+  const [Identity, setIdentity] = useState();
+  const [Image, setImage] = useState();
 
-    const {id}= useParams();
-    return (
-        <div>
-            Edit {id}
-        </div>
-    );
-}
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem("token_user");
+
+  const getUser = () => {
+    fetch(import.meta.env.VITE_APIURL + "Doctors/" + id, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setName(data.data.name_Doctor);
+        setLastName(data.data.lastName_Doctor);
+        setMail(data.data.email_Doctor);
+        setPhone(data.data.phone_Doctor);
+        setIdentity(data.data.identity_Doctor);
+        setImage(data.data.img_Doctor);
+      });
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const Update=(name,lastname,mail,phone,identity,image)=>{
+    console.log(name,lastname,mail,phone,identity,image)
+  }
+  return (
+    <div>
+      Edit {id}
+      <Button onClick={() => navigate("/doctors")}>{"<-"}Back</Button>
+      <Paper>
+        <Grid
+          container
+          direction={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <TextField type="text" placeholder="name..." fullWidth value={Name} onChange={(e)=>setName(e.target.value)}/>
+          <TextField type="text" placeholder="lastname..." fullWidth value={LastName} onChange={(e)=>setLastName(e.target.value)}/>
+          <TextField type="text" placeholder="mail..." fullWidth value={Mail} onChange={(e)=>setMail(e.target.value)}/>
+          <TextField type="text" placeholder="phone..." fullWidth value={Phone} onChange={(e)=>setPhone(e.target.value)}/>
+          <TextField type="text" placeholder="identity..." fullWidth value={Identity} onChange={(e)=>Identity(e.target.value)}/>
+          <TextField type="file" />
+
+          <Button onClick={()=>Update(Name,LastName,Mail,Phone,Identity,Image)}>Save</Button>
+        </Grid>
+      </Paper>
+    </div>
+  );
+};
 
 export default EditDoctor;
